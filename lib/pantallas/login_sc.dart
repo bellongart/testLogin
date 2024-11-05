d6071a151f24325f852ee90bc48b258e533f8524
homet <bellolongartj@gmail.com>
2024-11-05 08:29:29 -0400

    correcion del desbordamiento del login

--- lib/pantallas/login_sc.dart
+++ lib/pantallas/login_sc.dart
@@ -15,10 +15,11 @@ class _LognScState extends State<LognSc> {
   @override
   Widget build(BuildContext context) {
     const styleLgn = TextStyle(fontSize: 20.0, color: Colors.white);
-    const styLitt = TextStyle(fontSize: 12.0, color: Color.fromARGB(255, 158, 158, 158));
+    const styLitt =
+        TextStyle(fontSize: 12.0, color: Color.fromARGB(255, 158, 158, 158));
     const colPrim = Color(0xFF004F8F);
     const colSec = Color.fromARGB(255, 216, 130, 0);
-    double tamAnch = MediaQuery.of(context).size.width*0.25;
+    double tamAnch = MediaQuery.of(context).size.width * 0.25;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: colSec,
@@ -34,45 +35,45 @@ class _LognScState extends State<LognSc> {
             key: _formKey,
             child: Column(
               children: [
-                Expanded(
+                SingleChildScrollView(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       const Text('Inicie sesión',
-                        style: TextStyle(
-                          fontSize: 25.0, color: Colors.white)),
+                          style:
+                              TextStyle(fontSize: 25.0, color: Colors.white)),
                       const Text('Cédula de identidad', style: styleLgn),
-                        SizedBox(width: tamAnch),
-                        TextFormField(
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            hintText: 'Ingresar cédula de identidad',
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: OutlineInputBorder(),
-                          ),
-                          validator: (ci) {
-                            if(ci == null || ci.isEmpty){
-                              return 'Ingrese su cédula';
-                            }
-                            return null;
-                          },
+                      SizedBox(width: tamAnch),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: 'Ingresar cédula de identidad',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: OutlineInputBorder(),
                         ),
-                        const Text('Contraseña', style: styleLgn),
-                          SizedBox(width: tamAnch),
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: const InputDecoration(
-                              hintText: 'Ingresar contraseña',
-                              hintStyle: TextStyle(color: Colors.grey),
-                              border: OutlineInputBorder()),
-                            validator: (pasw) {
-                              if (pasw! == null || pasw.isEmpty) {
-                                return 'Ingrese una contraseña válida';
-                              }
-                              return null;
-                            },
-                          ),
+                        validator: (ci) {
+                          if (ci == null || ci.isEmpty) {
+                            return 'Ingrese su cédula';
+                          }
+                          return null;
+                        },
+                      ),
+                      const Text('Contraseña', style: styleLgn),
+                      SizedBox(width: tamAnch),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                            hintText: 'Ingresar contraseña',
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: OutlineInputBorder()),
+                        validator: (pasw) {
+                          if (pasw! == null || pasw.isEmpty) {
+                            return 'Ingrese una contraseña válida';
+                          }
+                          return null;
+                        },
+                      ),
                     ],
                   ),
                 ),
@@ -83,7 +84,6 @@ class _LognScState extends State<LognSc> {
                       TextButton(
                         onPressed: null,
                         child: Text('Olvidé contraseña', style: styLitt),
-        
                       ),
                     ],
                   ),
@@ -103,7 +103,7 @@ class _LognScState extends State<LognSc> {
         //     builder: (context) => const HomePage(),
         //     ),
         //   );
-        // },         
+        // },
         backgroundColor: colSec,
         foregroundColor: colPrim,
         child: const Icon(Icons.arrow_forward),
